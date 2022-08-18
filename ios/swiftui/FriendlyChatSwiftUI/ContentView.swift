a81cf4069d9235233de40a4fddef7eab77bea3f2
Rachel Collins <racheldosh@google.com>
2022-08-17 18:03:13 -0700

    Creating new repo swiftui for codelab updates

---
+++ ios/swiftui/FriendlyChatSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FriendlyChatSwiftUI
+//
+//  Created by Rachel Collins on 8/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/swiftui/FriendlyChatSwiftUI/FriendlyChatSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendlyChatSwiftUIApp.swift
+//  FriendlyChatSwiftUI
+//
+//  Created by Rachel Collins on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendlyChatSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
